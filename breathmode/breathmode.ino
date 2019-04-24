acc63a4f07ee057432e874446f00512b9238abce
G. Çağlar Çoban <g.caglar.c@gmail.com>
2019-04-24 20:39:42 +0300

    Initial commit

---
+++ breathmode/breathmode.ino
@@ -0,0 +1,57 @@
+
+#include <IRremote.h>
+
+
+const int redPin = 9;
+const int greenPin = 5;
+const int bluePin = 6;
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+
+
+void setup() {
+  // Start off with the LED off.
+  Serial.begin(9600);
+  // In case the interrupt driver crashes on setup, give a clue
+  // to the user what's going on.
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
+    
+}
+
+if (irrecv.decode(&results)) {
+      if (results.value == 17||results.value == 2065)
+
+  void loop() {
+    unsigned int rgbColour[3];
+
+  // Start off with red.
+  rgbColour[0] = 255;
+  rgbColour[1] = 0;
+  rgbColour[2] = 0;  
+
+  // Choose the colours to increment and decrement.
+  for (int decColour = 0; decColour < 3; decColour += 1) {
+    int incColour = decColour == 2 ? 0 : decColour + 1;
+
+    // cross-fade the two colours.
+    for(int i = 0; i < 255; i += 1) {
+      rgbColour[decColour] -= 1;
+      rgbColour[incColour] += 1;
+      
+      setColourRgb(rgbColour[0], rgbColour[1], rgbColour[2]);
+      delay(5);
+          }
+      }
+      }
+
+  void setColourRgb(unsigned int red, unsigned int green, unsigned int blue) {
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+    }
+} 
